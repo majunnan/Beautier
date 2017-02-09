5eac33fb1a00cf07bca045e6d2739c54e2a14789
Yammy <yammyxing@foxmail.com>
2017-02-09 23:51:05 +0800

    add name to signuo/login page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "这个seeds文件可以自动建立一个admin账号和一个test账号"
+
+create_account = User.create([name:'admin', email: 'admin@gmail.com', password:'111111', password_confirmation: '111111', is_admin: 'true'])
+puts "Admin account created."
+
+create_account = User.create([name:'test', email: 'test@gmail.com', password:'111111', password_confirmation: '111111', is_admin: 'false'])
+puts "Test account created."
