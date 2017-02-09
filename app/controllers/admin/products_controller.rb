5a0e5b620d4bb1b4f1e1a011ab964ed7a796599b
Yammy <yammyxing@foxmail.com>
2017-02-10 00:35:36 +0800

    admin can upload image after add_image_to_product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -45,6 +45,6 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :descriptin, :quantity, :price)
+    params.require(:product).permit(:title, :descriptin, :quantity, :price, :image)
   end
 end
