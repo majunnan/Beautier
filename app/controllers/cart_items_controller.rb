05f386e42bfa198370d147b305d9fa7925717070
Yammy <yammyxing@foxmail.com>
2017-02-10 14:40:07 +0800

    user cannot add item with 0 quantity and update quantity if ask more than quantity

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,6 +1,18 @@
 class CartItemsController < ApplicationController
   before_action :authenticate_user!
 
+  def update
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+      @cart_item.update(cart_item_params)
+      flash[:notice] = "You've successfully change the number!"
+    else
+      flash[:notice] = "The quantity is not enough to add to cart"
+    end
+    redirect_to :back
+  end
+
   def destroy
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
@@ -8,5 +20,11 @@ class CartItemsController < ApplicationController
     @product = @cart_item.product
     redirect_to :back
     flash[:alert] = "You've deleted the #{@product.title} from the cart!"
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
   end
 end
