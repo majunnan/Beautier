eb90abc8997b0f10694d1d79709d68f44798cbb1
Yammy <yammyxing@foxmail.com>
2017-02-10 14:16:41 +0800

    user cannot add the same item to cart twice

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,8 +9,12 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_product_to_cart(@product)
-    flash[:notice] = "Successfully add it to cart!"
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "Successfully add #{@product.title} to cart!"
+    else
+      flash[:warning] = "You've already add it to cart!"
+    end
     redirect_to :back
   end
 end
